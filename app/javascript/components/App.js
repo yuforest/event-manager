eff17ed1ae7a37474d87b6fc8df90cfa73034012
大森優太 <>
2022-05-30 23:01:50 +0900

    feat: react rails tutorial

---
+++ app/javascript/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Editor from './Editor';
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+
+const App = () => (
+  <>
+    <Routes>
+      <Route path="events/*" element={<Editor />} />
+    </Routes>
+    <ToastContainer />
+  </>
+);
+
+export default App;
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'site/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: redirect('/events')
+
+  get 'events', to: 'site#index'
+  get 'events/new', to: 'site#index'
+  get 'events/:id', to: 'site#index'
+  get 'events/:id/edit', to: 'site#index'
+
+  namespace :api do
+    resources :events, only: %i[index show create destroy update]
+  end
+end
